b624c5b87d7a429fcedbd1783e955ef7e2fcfdd5
Damien Guard <damieng@gmail.com>
2019-02-07 16:18:58 -0800

    Remove redundant using

--- StripeSubscription/StripeSubscriptionFunction.cs
+++ StripeSubscription/StripeSubscriptionFunction.cs
@@ -2,7 +2,6 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Stripe;
